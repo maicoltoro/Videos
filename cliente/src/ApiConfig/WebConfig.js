dd04c64fe39c15907f1080d355763e932fb8f388
maicol <maicolestegantoro@gmail.com>
2023-05-12 07:45:42 -0500

    Mensaje descriptivo de los cambios

--- cliente/src/ApiConfig/WebConfig.js
+++ cliente/src/ApiConfig/WebConfig.js
@@ -1,14 +1,19 @@
 import axios from  'axios'
 
-// export const api = axios.create({
-//     baseURL: 'http://192.168.132.129:5000/'
-// })
-
 export const api = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'http://192.168.132.129:5000/'
 })
 
+// export const api = axios.create({
+//     baseURL: 'http://localhost:5000/'
+// })
+
+// export const apiEnvio = axios.create({
+//     baseURL: 'http://localhost:5000/',
+//     responseType: 'arraybuffer', 
+// });
+
 export const apiEnvio = axios.create({
-    baseURL: 'http://localhost:5000/',
+    baseURL: 'http://192.168.132.129:5000/',
     responseType: 'arraybuffer', 
 });
